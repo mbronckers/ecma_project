4cf987582423f98a521970ee84a023531ef172f3
Dustin Zhang <dustinzhang@cs.uchicago.edu>
2021-05-13 15:31:12 -0500

    Added more configurations to dgp and updated functions.py correspondingly

--- functions.py
+++ functions.py
@@ -7,14 +7,11 @@ Execute heterogeneous treatment on data
 
 Returns: X, X@betas
 """
-def heterogeneousTreatment(X, betas):
+def heterogenousTreatment(X, treatments, betas, effect):
     xb = X@betas
-    treatments = np.random.randint(2, size=X.shape[0]).reshape(
-        (-1, 1))  # randomly assigned treatments with propensity 0.5
     for i in range(X.shape[0]):
         if int(treatments[i]) > 0:
-            # heterogenous treatment is 1 + 2*x_2*x_5
-            xb[i] += 1 + 2 * X[i][1] * X[i][4]
+            xb[i] += 1 + effect * X[i][1] * X[i][4] # heterogenous treatment is 1 + 2*x_2*x_5
     X = np.append(X, treatments, axis=1)
     return X, xb
 
@@ -23,59 +20,54 @@ Data Generating Process
     Configure treatment effect heterogeneity, independent of regression form and sample size
 
 Params:
-    treatment - heterogeneous vs homogeneous 
-    cc - number of continuous covariates to be included (pre-introduction of third order interactions)
-    linear - 
-
+    effect_type - heterogeneous vs homogeneous
+    effect_homogenous - if effect_type is homogenous, controls what the treatment coefficient is
+    effect_heterogenous - if effect_type is heterogenous, controls n where the treatment coefficient is 1 + n * x_2 * x_5
+    treatment_probability - probability that an observation is assigned the treatment, default is 0.5
+    order - order of interactions between covariates desired, as order is increased, number of covariates and nonlinearity (if specified) is also increased
+    linear - flag to control whether the dgp has exponents larger than 1 
+    cc - number of continuous covariates to be included (pre-introduction of n order interactions)
+    N - sample size
+    rho - variance between continuous covariates
 Returns: y, X, betas, features
 """
-def dgp(treatment="heterogeneous", linear=False, cc=4, N=1000, rho=0.2):
+def dgp(effect_type="heterogenous", effect_homogenous=3, effect_heterogenous=2, treatment_probability=0.5, order=3, linear=False, cc=4, N=1000, rho=0.2):
     error = np.random.normal(size=(N,1))
     
-    added_covariates = 2
+    # linear boolean will be used later to determine whether we want to include polynomial features
+    # at this point, we add two more continuous covariates if linear to have approximately same 
+    # number of covariates between the linear and nonlinear cases
     if linear: 
-        # linear => include 2 more continuous covariates
-        # to have approximately same number of covariates between the linear and nonlinear cases
-        cov = (np.eye(cc+added_covariates) * (1-rho)) + (np.ones((cc+added_covariates, cc+added_covariates)) * rho)
-        
-        # generate N continuous covariates of X
-        X = np.random.multivariate_normal(np.zeros(cc+added_covariates),
-                                         cov, size=N, check_valid='warn', tol=1e-8) 
+        cov = (np.eye(cc+2) * (1-rho)) + (np.ones((cc+2, cc+2)) * rho)
+        X = np.random.multivariate_normal(np.zeros(cc+2), cov, size=N, check_valid='warn', tol=1e-8) # generate 4 continuous covariates of X
     else:
         cov = (np.eye(cc) * (1-rho)) + (np.ones((cc, cc)) * rho)
-        
-        # generate N continuous covariates of X
-        X = np.random.multivariate_normal(np.zeros(cc),
-         cov, size=N, check_valid='warn', tol=1e-8) 
+        X = np.random.multivariate_normal(np.zeros(cc), cov, size=N, check_valid='warn', tol=1e-8) # generate 4 continuous covariates of X
 
-    # add binary [0, 1] covariate
-    X = np.append(X, np.random.randint(2, size=N).reshape((-1, 1)), axis=1)
+    X = np.append(X, np.random.randint(2, size=N).reshape((-1, 1)), axis=1) # add binary [0, 1] covariate
 
-    elements = [1, 2, 3]
-    probabilities = [0.2, 0.5, 0.3] # arbitrary
-    
-    # add unordered categorical [1, 2, 3] covariate
-    X = np.append(X, np.random.choice(elements, N, p=probabilities).reshape((-1, 1)), axis=1) 
+    cat_elements = [1, 2, 3]
+    cat_probabilities = [0.2, 0.5, 0.3] # arbitrary
+    X = np.append(X, np.random.choice(cat_elements, size=N, p=cat_probabilities).reshape((-1, 1)), axis=1) # add unordered categorical [1, 2, 3] covariate
 
-    # add third order interactions if linear is false for X covariates
-    # increases number of covariates (to have a high dimensional dataset)
-    poly = PolynomialFeatures(3, interaction_only=linear) 
+    poly = PolynomialFeatures(order, interaction_only=linear) # add third order interactions if linear is false for X covariates, increases number of covariates (to have a high dimensional dataset)
     X = poly.fit_transform(X)
     X = X[:,1:] # drop the constant term
     features = (poly.get_feature_names()[1:])
     features.append("w")
 
-    # randomly assigned treatments with propensity 0.5
-    treatments = np.random.randint(2, size=N).reshape((-1, 1)) 
+    treat_elements = [0, 1]
+    treat_probabilities = [1 - treatment_probability, treatment_probability]
+    treatments = np.random.choice(treat_elements, size=N, p=treat_probabilities).reshape((-1, 1)) # randomly assigned treatments with propensity 0.5
     
-    # heterogenous vs. homogenous treatments
-    if treatment == "homogeneous":
-        betas = np.append(np.random.normal(size=X.shape[1]), [3]).reshape(-1,1)
+    # configure between heterogenous and homogenous treatments
+    if effect_type == "homogenous":
+        betas = np.append(np.random.normal(size=X.shape[1]), [effect_homogenous]).reshape(-1,1)
         X = np.append(X, treatments, axis=1)
         xb = X@betas
     else:
         betas = np.random.normal(size=X.shape[1]).reshape(-1,1)
-        X, xb = heterogeneousTreatment(X, betas)
+        X, xb = heterogenousTreatment(X, treatments, betas, effect_heterogenous)
 
-    y = xb + error
+    y = xb + error 
     return y, X, betas, features
